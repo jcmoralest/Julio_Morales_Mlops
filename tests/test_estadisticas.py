0c3a8999bd2085c458443cb56ab948ed48deab31
Julio Morales <julioscorpion.007@gmail.com>
2025-05-17 16:32:52 -0500

    CI/CD terminado con éxito.

--- tests/test_estadisticas.py
+++ tests/test_estadisticas.py
@@ -10,10 +10,37 @@ if os.path.exists(log_path):
 from main import simular_prediccion, registrar_prediccion, leer_estadisticas
 
 def test_prediccion_y_estadisticas():
-    sintomas = {"fiebre": 8, "tos": 8, "dolor": 8}
+    sintomas = {"fiebre": 3, "tos": 2, "dolor": 2}
     resultado = simular_prediccion(sintomas)
     registrar_prediccion(resultado, sintomas)
     estadisticas = leer_estadisticas()
+    # Verifica que la última predicción registrada sea la esperada
+    assert estadisticas["fecha_ultima_prediccion"] is not None
+    assert estadisticas["ultimas_5_predicciones"][-1]["categoria"] == resultado
+    print("Ultima predicción registrada:", estadisticas["ultimas_5_predicciones"][-1])
+    print("Conteo por categoría:", estadisticas["conteo_por_categoria"])
+    print("Fecha de la última predicción:", estadisticas["fecha_ultima_prediccion"])
+
+    sintomas = {"fiebre": 8, "tos": 8, "dolor": 7}
+    resultado = simular_prediccion(sintomas)
+    registrar_prediccion(resultado, sintomas)
+    estadisticas = leer_estadisticas()
+    # Verifica que la última predicción registrada sea la esperada
+    assert estadisticas["fecha_ultima_prediccion"] is not None
+    assert estadisticas["ultimas_5_predicciones"][-1]["categoria"] == resultado
+    print("Ultima predicción registrada:", estadisticas["ultimas_5_predicciones"][-1])
+    print("Conteo por categoría:", estadisticas["conteo_por_categoria"])
+    print("Fecha de la última predicción:", estadisticas["fecha_ultima_prediccion"])
+
+    sintomas = {"fiebre": 5, "tos": 5, "dolor": 5}
+    resultado = simular_prediccion(sintomas)
+    registrar_prediccion(resultado, sintomas)
+    estadisticas = leer_estadisticas()
+    
+    print("Ultima predicción registrada:", estadisticas["ultimas_5_predicciones"][-1])
+    print("Conteo por categoría:", estadisticas["conteo_por_categoria"])
+    print("Fecha de la última predicción:", estadisticas["fecha_ultima_prediccion"])
+    
     # Verifica que la última predicción registrada sea la esperada
     assert estadisticas["fecha_ultima_prediccion"] is not None
     assert estadisticas["ultimas_5_predicciones"][-1]["categoria"] == resultado
